ba87ae1571bed473e89545fd7ce5ea56fc732acf
Anton S <e2h3in@gmail.com>
2021-11-20 15:10:46 +0100

    fucntions

--- functions/server/index.js
+++
@@ -1,7 +0,0 @@
-require = require("esm")(module /* , options*/);
-
-const { Server } = require("./main");
-
-const handler = Server.createHandler()
-
-module.exports = { handler }
--- functions/server/resolvers/index.js
+++
@@ -1,9 +0,0 @@
-import posts from "./posts";
-import users from "./users";
-import email from "./email";
-
-export default [
-    posts,
-    users,
-    email
-]
