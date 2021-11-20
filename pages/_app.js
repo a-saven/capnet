8563537b438c0244c7ae1bebbea630f5df17ced3
Anton S <e2h3in@gmail.com>
2021-11-20 15:15:51 +0100

    Back

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,7 @@
-import Head from 'next/head'
-import ErrorBoundary from 'src/components/utils/errorBoundary'
-import { SessionProvider } from 'next-auth/react'
-import ThemeProvider from 'src/theme'
-import { ApolloProvider } from '@apollo/client'
-import client from 'client/apollo-client'
+import '@styles/globals.css'
 
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps }
-}) {
-  return (
-    <>
-      <Head>
-        <title>Captain Log</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-
-      <ErrorBoundary>
-        <ThemeProvider>
-          <ApolloProvider client={client}>
-            <SessionProvider session={session}>
-              <Component {...pageProps} />
-            </SessionProvider>
-          </ApolloProvider>
-        </ThemeProvider>
-      </ErrorBoundary>
-    </>
-  )
+function Application({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
+
+export default Application
